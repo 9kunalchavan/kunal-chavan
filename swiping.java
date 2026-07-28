191eaf98e2d3474ebc3dff3cce9acbf853f7652a
Kunal Chavan <kunalchavan00007@gmail.com>
2026-07-28 12:13:15 +0530

    swiping numbers #java

---
+++ swiping.java
@@ -0,0 +1,16 @@
+package java1;
+
+public class swiping {
+	public static void main(String[] args) {
+		int a = 99;
+		int b = 77;
+
+		int temp = a; 
+		a = b;        
+		b = temp;   
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
