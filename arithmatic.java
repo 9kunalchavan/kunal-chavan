43fa1e56bb693fdf9718c7c4f3349eaf8eb7b541
Kunal Chavan <kunalchavan00007@gmail.com>
2026-07-28 12:12:36 +0530

    arithmatic operations

---
+++ arithmatic.java
@@ -0,0 +1,21 @@
+package java1;
+
+public class arithmatic {
+	public static void main(String[] args) {
+		int a=10;
+		int b=4;
+		
+		int sum =a+b;
+		System.out.println("(a+b)"+sum);
+		
+		int sub =a-b;
+		System.out.println("(a-b)"+sub);
+		
+		int multiply=a*b;
+		System.out.println("(a*b)"+multiply);
+		
+		int divide=a/b;
+		System.out.println("(a/b)"+divide);
+	}
+
+}
